1a1b7a7851fc4a95fadc6b17be8b25a073bc12a8
infopropertymg <infopropertymg@gmail.com>
2025-02-05 21:16:10 +0100

    script.js

--- script.js
+++ script.js
@@ -18,19 +18,3 @@ zoomImages.forEach((image) => {
     image.addEventListener('mouseleave', () => {
         zoomedImageContainer.style.display = 'none'; // Hide the zoomed image container
     });
-});
-// Get the menu icon and dropdown menu
-const menuIcon = document.getElementById('menu-icon');
-const dropdownMenu = document.getElementById('dropdown-menu');
-
-// Toggle the dropdown menu on click
-menuIcon.addEventListener('click', () => {
-    dropdownMenu.classList.toggle('active');
-});
-
-// Close the dropdown menu when clicking outside
-document.addEventListener('click', (event) => {
-    if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
-        dropdownMenu.classList.remove('active');
-    }
-});
